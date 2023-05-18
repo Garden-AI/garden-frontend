5265cb2292c603fec99bc3c79e42be1be8e7accc
Will Engler <Engler.Will@gmail.com>
2023-05-18 12:54:18 -0500

    only need search scope, no ingest or gcs

--- garden/src/constants.ts
+++ garden/src/constants.ts
@@ -1,5 +1,5 @@
 const GLOBUS_NATIVE_CLIENT_ID = "1fbb4126-1e48-4f30-8c0f-d71c2715c244"
-const SEARCH_SCOPE = "urn:globus:auth:scope:search.api.globus.org:all"
+const SEARCH_SCOPE = "urn:globus:auth:scope:search.api.globus.org:search"
 const SEARCH_ENDPOINT = "https://search.api.globus.org/"
 const GARDEN_INDEX_UUID = "58e4df29-4492-4e7d-9317-b27eba62a911"
 const GARDEN_INDEX_URL = `${SEARCH_ENDPOINT}/v1/index/${GARDEN_INDEX_UUID}`;
