31d2379914317b0b924df875d6c4939aa7b633b6
chasejenkins303 <chasejenkins303@gmail.com>
2023-06-06 14:39:27 -0400

    bottom section stack vertical when screen is small

--- garden/src/pages/HomePage.tsx
+++ garden/src/pages/HomePage.tsx
@@ -141,17 +141,21 @@ const HomePage = () => {
 
       <div className="font-display">Featured Gardens</div>
 
+
+        {/* Bottom section/credits */}
       <div className="font-display bg-green pb-8">
-        <p className="text-white text-xl pt-8 text-center">
+        <p className="text-white text-lg sm:text-xl pt-8 text-center">
           This project was made possible by:
         </p>
-        <div className="bg-white my-8 mx-16 rounded-xl flex items-center justify-evenly py-10">
+        <div className="bg-white my-8 mx-6 sm:mx-16 md:mx-16 rounded-xl md:flex items-center justify-evenly py-10">
           <section className="flex flex-col items-center">
             <a target="blank" href="https://materialsdatafacility.org/">
               <img src="img/MDF-logo.png" className="max-w-[200px]" />
             </a>
-            <h1 className="font-bold text-xl pt-6 text-center"> MDF </h1>
-            <p className="text-center mx-5 pt-4 max-w-xs">
+            <h1 className="font-bold text-md md:text-xl pt-6 text-center">
+              MDF
+            </h1>
+            <p className="text-center text-sm md:text-lg mx-1 md:mx-5 pt-2 pb-8 md:pt-4 max-w-xs">
               Data services to help material scientists publish and discover
               data
             </p>
@@ -161,14 +165,13 @@ const HomePage = () => {
             <a target="blank" href="https://www.nsf.gov/">
               <img
                 src="img/National-Science-Foundation-logo.jpeg"
-                className="max-w-[90px]"
+                className="max-w-[70px]"
               />
             </a>
-            <h1 className="font-bold text-xl pt-6 text-center">
-              {" "}
-              National Science Foundation{" "}
+            <h1 className="font-bold text-md md:text-xl pt-6 text-center">
+              National Science Foundation
             </h1>
-            <p className="text-center mx-5 pt-4 max-w-xs text-sm">
+            <p className="text-center mx-1 md:mx-5 pt-4 max-w-xs text-sm pb-8">
               Award Abstract #2209892: “Frameworks: Garden: A FAIR Framework for
               Publishing and Applying AI Models for Translational Research in
               Science, Engineering, Education, and Industry”
@@ -179,8 +182,8 @@ const HomePage = () => {
             <a target="blank" href="https://foundry-ml.org/#/">
               <img src="img/foundry-logo.png" className="max-w-[200px]" />
             </a>
-            <h1 className="font-bold text-xl pt-6 text-center"> Foundry </h1>
-            <p className="text-center mx-5 pt-4 max-w-xs">
+            <h1 className="font-bold text-md md:text-xl pt-6 text-center"> Foundry </h1>
+            <p className="text-center text-sm md:text-lg mx-5 pt-4 max-w-xs">
               An open source machine learning platform for scientists
             </p>
           </section>
