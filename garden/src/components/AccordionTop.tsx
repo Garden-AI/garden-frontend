f34fe1cb5ee2a16e0d2b93e3c4ad8dae1986aa28
chasejenkins303 <chasejenkins303@gmail.com>
2024-03-20 10:44:07 -0400

    papers link, dataset name change

--- garden/src/components/AccordionTop.tsx
+++ garden/src/components/AccordionTop.tsx
@@ -102,8 +102,13 @@ const AccordionTop = ({ entrypoint }: { entrypoint: any }) => {
                             d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
                           />
                         </svg>
-
+                        { paper.doi ?
+                        <a href={'https://www.doi.org/'+paper.doi} target="_blank">
+                          <h1 className="text-2xl w-full break-words">{paper.title}</h1>
+                        </a>
+                        :
                         <h1 className="text-2xl w-full break-words">{paper.title}</h1>
+                        }
                       </div>
                       <div className="px-2 py-2">
                         
