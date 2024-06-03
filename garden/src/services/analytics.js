979223ad97160e243aa6843b944086337ac374cf
Shane Jung <shanejung12@gmail.com>
2024-06-03 13:20:02 -0500

    Extracts Google Analytics functionality to a new file.
    Modifies the HashRouter creation to allow for easier integration with Google Analytics.

---
+++ garden/src/services/analytics.js
@@ -0,0 +1,25 @@
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const TRACKING_ID = "G-99GHD3HEZP"; // May move this to an environment variable later, perhaps different for dev and prod
+
+// Initialize Google Analytics
+ReactGA.initialize(TRACKING_ID);
+
+const trackPageView = () => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.hash,
+  });
+  console.log("Page view tracked");
+};
+
+const useGoogleAnalytics = () => {
+  const location = useLocation();
+  useEffect(() => {
+    trackPageView();
+  }, [location]);
+};
+
+export default useGoogleAnalytics;
