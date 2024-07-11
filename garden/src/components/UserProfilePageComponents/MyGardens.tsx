837507288240efcc4a8ed8a657e6a2fa709f0f73
jamies31 <89811255+jamies31@users.noreply.github.com>
2024-07-11 09:04:45 -0700

    switching

--- garden/src/components/UserProfilePageComponents/MyGardens.tsx
+++ garden/src/components/UserProfilePageComponents/MyGardens.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Garden } from "../../types";
 import GardenBox from "@/components/GardenBox";
 import { useSearchGardens } from "../../api/search";
+import { Link } from 'react-router-dom';
 
 
 const MyGardens = () => {
@@ -37,14 +38,28 @@ const MyGardens = () => {
     return (
         <div className="">
             <div className="flex justify-end mb-4">
-                <button className="flex flex-row items-center gap-2 rounded-lg border border-gray-200 px-2 py-1 text-sm mb-6">
-                    <span className="text-black">Create New Garden</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="green" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-plus">
-                        <circle cx="12" cy="12" r="10" />
-                        <path d="M8 12h8" />
-                        <path d="M12 8v8" />
-                    </svg>
-                </button>
+            <Link
+            to="/garden/create"
+            className="flex flex-row items-center gap-2 rounded-lg border border-gray-200 px-2 py-1 text-sm mb-6"
+            >
+            <span className="text-black">Create New Garden</span>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="green"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-circle-plus"
+            >
+                <circle cx="12" cy="12" r="10" />
+                <path d="M8 12h8" />
+                <path d="M12 8v8" />
+            </svg>
+            </Link>
             </div>
             <div className="mb-6">
                 {isLoading ? (
