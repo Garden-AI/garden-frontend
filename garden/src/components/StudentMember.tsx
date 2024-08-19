057abfd303c9de85ede963a362baff147a09d14c
pillkim <pmkim08@gmail.com>
2024-08-19 10:54:58 -0400

    icons stay at bottom of cards

--- garden/src/components/StudentMember.tsx
+++ garden/src/components/StudentMember.tsx
@@ -1,15 +1,17 @@
 const StudentMember = ({ member }: { member: any }) => {
   return (
-    <div className="flex flex-col rounded-lg border border-gray-200 p-5 shadow-sm">
-      <p className="text-xl font-bold">{member.name}</p>
-      <p className="text-sm italic text-gray-600">{member.major}</p>
-      <div className="flex gap-5">
+    <div className="flex flex-1 flex-col justify-between rounded-lg border border-gray-200 p-4 shadow-sm">
+      <div>
+        <p className="text-xl font-bold">{member.name}</p>
+        <p className="text-sm italic text-gray-600">{member.major}</p>
+      </div>
+      <div className="">
         {member.linkedin_link.length > 0 ? (
           <a
             href={member.linkedin_link}
             target="_blank"
             rel="noreferrer"
-            className="pt-5"
+            className="inline-block pt-2 align-bottom"
           >
             <img
               className="h-5 w-5"
@@ -25,7 +27,7 @@ const StudentMember = ({ member }: { member: any }) => {
             href={member.github_link}
             target="_blank"
             rel="noreferrer"
-            className="pt-5"
+            className="inline-block px-2 pt-2 align-bottom"
           >
             <img
               className="h-5 w-5"
--- garden/src/components/TeamMember.tsx
+++ garden/src/components/TeamMember.tsx
@@ -1,51 +1,53 @@
 const TeamMember = ({ member }: { member: any }) => {
   return (
-    <div className="rounded-lg border border-gray-200 p-5 shadow-sm">
-      <img
-        className="mx-auto h-36 w-auto rounded-full"
-        src={member.photo}
-        alt={member.name}
-      />
-      <div className="mt-2 flex flex-col items-center">
-        <p className="text-xl font-bold">{member.name}</p>
-        <p className="text-center text-sm italic text-gray-600">
-          {member.workplace}, {member.title}
-        </p>
-        {/* <p>{member.title}</p> */}
-        <div className="flex gap-4">
-          {member.linkedin_link.length > 0 ? (
-            <a
-              href={member.linkedin_link}
-              target="_blank"
-              rel="noreferrer"
-              className="pt-2"
-            >
-              <img
-                className="h-5 w-5"
-                src="img/LinkedIn-logo.png"
-                alt="linkedin logo"
-              />
-            </a>
-          ) : (
-            <></>
-          )}
-          {member.twitter_link.length > 0 ? (
-            <a
-              href={member.twitter_link}
-              target="_blank"
-              rel="noreferrer"
-              className="pt-2"
-            >
-              <img
-                className="h-5 w-5"
-                src="img/twitter-logo.png"
-                alt="twitter logo"
-              />
-            </a>
-          ) : (
-            <></>
-          )}
+    <div className="flex flex-1 flex-col justify-between rounded-lg border border-gray-200 p-5 shadow-sm">
+      <div className="">
+        <img
+          className="mx-auto h-36 w-auto rounded-full"
+          src={member.photo}
+          alt={member.name}
+        />
+        <div className="mt-2 flex flex-col items-center">
+          <p className="text-xl font-bold">{member.name}</p>
+          <p className="text-center text-sm italic text-gray-600">
+            {member.workplace}, {member.title}
+          </p>
         </div>
+        {/* <p>{member.title}</p> */}
+      </div>
+      <div className="mx-auto">
+        {member.linkedin_link.length > 0 ? (
+          <a
+            href={member.linkedin_link}
+            target="_blank"
+            rel="noreferrer"
+            className="mx-2 inline-block pt-2 align-bottom"
+          >
+            <img
+              className="h-5 w-5"
+              src="img/LinkedIn-logo.png"
+              alt="linkedin logo"
+            />
+          </a>
+        ) : (
+          <></>
+        )}
+        {member.twitter_link.length > 0 ? (
+          <a
+            href={member.twitter_link}
+            target="_blank"
+            rel="noreferrer"
+            className="mx-2 inline-block pt-2 align-bottom"
+          >
+            <img
+              className="h-5 w-5"
+              src="img/twitter-logo.png"
+              alt="twitter logo"
+            />
+          </a>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
