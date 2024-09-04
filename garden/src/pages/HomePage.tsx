85da6f5f757a1a520d030e57cd4da16e6997e335
Ben Blaiszik <blaiszik@uchicago.edu>
2024-09-03 23:35:23 -0500

    Fix svg camel case

--- garden/src/pages/HomePage.tsx
+++ garden/src/pages/HomePage.tsx
@@ -291,8 +291,8 @@ const HomePage = () => {
               target="_blank"
               className="mr-5 w-24 pb-4 pt-3 hover:text-gray-200 dark:hover:text-gray-400"
             >
-              <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            viewBox="0 0 1680 1040" enable-background="new 0 0 1680 1040" xml:space="preserve" class="fill-current">
+              <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 1680 1040" enableBackground="new 0 0 1680 1040" xmlSpace="preserve" className="fill-current">
           <path d="M880,880h160V400H880V880z M1120,320h400V160h-400V320z M880,160.00003h160v160H880V160.00003z M640,880h160V160H640V880z
             M400,720h160V160H400V720z M160,880h160V160H160V880z M1120,880h160V400h-160V880z"/>
           </svg>
