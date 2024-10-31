66c2bf146a18f19606a77c086302956acde0866f
shane-jung <71043373+shane-jung@users.noreply.github.com>
2024-10-31 13:18:11 -0500

    Basic setup for hiding Modal from from non-globus group users.

--- garden/src/components/form/garden/create/CreateGardenPage.tsx
+++ garden/src/components/form/garden/create/CreateGardenPage.tsx
@@ -1,25 +1,50 @@
-import { Separator } from "@/components/ui/separator";
 import { CreateGardenForm } from "./CreateGardenForm";
-import Breadcrumb from "@/components/Breadcrumb";
-import { useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import ScrollToTop from "@/components/ScrollToTop";
+import { useGetGlobusGroups } from "@/api/globus/useGetGlobusGroups";
 
 export default function CreateGardenPage() {
   const [searchParams, setSearchParams] = useSearchParams();
   const formType = searchParams.get("type");
 
+  const { data: groups } = useGetGlobusGroups();
+  const navigate = useNavigate();
+
+  if (
+    formType == "modal" &&
+    !groups?.find((group) => group.id === "53952f8a-d592-11ee-9957-193531752178") //todo: Replace with correct Globus Group ID
+  ) {
+    return (
+      <div className="mx-auto max-w-4xl px-8 py-24 font-display">
+        <div className="flex h-96 items-center justify-center ">
+          <div className="space-y-12 text-center">
+            <h1 className="text-4xl font-bold">Globus Group Required</h1>
+            <p className="text-gray-700">
+              You must be a part of the Globus Group to create a Modal App. Please email{" "}
+              <a href="mailto:wengler@uchicago.edu" className="font-bold text-primary">
+                Will Engler (wengler@chicago.edu)
+              </a>{" "}
+              to be added to the group.
+            </p>
+            <div className=" flex items-center justify-center space-x-4">
+              <Button onClick={() => navigate("/")} className="font-bold">
+                Back home
+              </Button>
+              <Button
+                onClick={() => setSearchParams({ type: "entrypoint" })}
+                className="font-bold"
+                variant={"outline"}
+              >
+                Create Garden from Entrypoints instead
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="mx-auto max-w-6xl px-8 py-16 font-display">
-      {/* <Breadcrumb
-        crumbs={[
-          { label: "Home", link: "/" },
-          { label: "Gardens", link: "/search" },
-          { label: "Create Garden" },
-        ]}
-      /> */}
-
       <CreateGardenFormHeader />
 
       {formType === null ? (
@@ -74,19 +99,8 @@ const CreateGardenFormHeader = () => {
   return (
     <>
       <div className="mb-12 flex items-center space-x-8">
-        {/* <div className="relative h-96 w-96 flex-shrink-0">
-          <img
-            src="img/AIGeneratedImg.png"
-            alt="Garden AI Logo"
-            className="absolute inset-0 h-full w-full rounded-lg object-cover"
-          />
-        </div> */}
         <div className="space-y-4">
           <h1 className="text-4xl font-light">Create a Garden</h1>
-          {/* <p className="text-sm text-gray-700">
-            Create a garden by uploading a modal app that you have created or by selecting one or
-            more entrypoints that you have created.
-          </p> */}
         </div>
       </div>
     </>
