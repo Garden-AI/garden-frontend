7e1206f0ac860abad034bb70a5e569a496e33921
shane-jung <71043373+shane-jung@users.noreply.github.com>
2024-11-05 12:58:56 -0600

    Initial setup for file validation See TODOs for items that need attention

--- garden/src/components/form/garden/create/UploadModalFormFields.tsx
+++ garden/src/components/form/garden/create/UploadModalFormFields.tsx
@@ -8,17 +8,18 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import ModalFunctions from "./ModalFunctions";
 import { fileToString } from "./utils";
 import { Link } from "react-router-dom";
 import React from "react";
 import LoadingSpinner from "@/components/LoadingSpinner";
-import { Button } from "@/components/ui/button";
+import { useValidateModalFile } from "@/api/modal/useValidateModalFile";
+import { toast } from "sonner";
 
 export const UploadModalFormFields = () => {
   const form = useFormContext();
   const [isFileUploading, setIsFileUploading] = React.useState(false);
+  const { mutateAsync: validateModalFile } = useValidateModalFile();
 
   const fileContents = form.watch("modal.file_contents");
 
@@ -29,18 +30,44 @@ export const UploadModalFormFields = () => {
     const file = e.target.files?.[0];
     if (file) {
       setIsFileUploading(true);
-      // TODO: Add call to backend that validates the file and returns the app name and functions
-      setTimeout(async () => {
-        // Simulate file upload (This will be removed when the above TODO is completed)
-        try {
-          const fileContents = await fileToString(file);
-          field.onChange(fileContents);
-        } catch (error) {
-          console.error("Error reading file:", error);
-        } finally {
-          setIsFileUploading(false);
+      try {
+        const { is_valid, functions, app_name, pip_requirements, base_image_requirements } =
+          await validateModalFile({ file });
+
+        if (!is_valid) {
+          field.onChange("");
+          form.setError("modal.file_contents", {
+            message: "Invalid modal file. Please see our user guide if you are having issues.",
+          });
+          throw new Error("Invalid modal file");
         }
-      }, 2000);
+
+        form.setValue("modal.app_name", app_name);
+
+        form.reset({ modal: { modal_functions: [] } });
+        form.setValue(
+          "modal.modal_functions",
+          functions.map((func) => ({
+            function_name: func.name,
+            description: "",
+            year: "2024",
+            is_archived: false,
+            doi: null,
+            title: "",
+            function_text: func.function_text,
+            authors: [],
+            tags: [],
+            test_functions: [],
+          })),
+        );
+
+        const fileContents = await fileToString(file);
+        field.onChange(fileContents);
+      } catch (error) {
+        console.error("Error reading file:", error);
+      } finally {
+        setIsFileUploading(false);
+      }
     }
   };
 
@@ -50,9 +77,11 @@ export const UploadModalFormFields = () => {
         <section>
           <h2 className="mb-2 text-2xl font-bold">Modal App</h2>
           <p className="mb-4 text-sm text-gray-500">
-            If you have a Python file that defines a Modal App, you can upload it here. The file
-            should contain a modal.App() definition. Please see our{" "}
-            <Link to="" className="font-bold text-primary">
+            Upload a Python file that defines your Modal App. Please see our{" "}
+            <Link
+              to="https://garden-ai.readthedocs.io/en/latest/user_guide/modal-publishing/"
+              className="font-bold text-primary"
+            >
               user guide
             </Link>{" "}
             for more information.{" "}
@@ -78,20 +107,7 @@ export const UploadModalFormFields = () => {
                       className="h-14 file:mr-4 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-violet-700 hover:file:bg-violet-100"
                     />
                   </FormControl>
-                  {field.value && (
-                    <Button
-                      type="button"
-                      variant="link"
-                      onClick={() => {
-                        form.reset({
-                          modal: { file_contents: "", app_name: "", modal_functions: [] },
-                        });
-                      }}
-                    >
-                      Clear
-                    </Button>
-                  )}
-                  <FormDescription>Your modal file containing your app definition</FormDescription>
+                  <FormDescription>Your modal file containing your app definition.</FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
@@ -105,31 +121,30 @@ export const UploadModalFormFields = () => {
               </div>
             ) : (
               fileContents && (
-                <div>
-                  <div className="space-y-6">
-                    <FormField
-                      control={form.control}
-                      name="modal.app_name"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel className="font-bold">App Name</FormLabel>
-                          <FormControl>
-                            <Input
-                              {...field}
-                              type="text"
-                              placeholder="my-modal-app"
-                              className="w-full"
-                            />
-                          </FormControl>
-                          <FormDescription>
-                            The name of your Modal App. It is the string inside modal.App() in your
-                            Python file.
-                          </FormDescription>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
+                <div className="">
+                  <FormField
+                    control={form.control}
+                    name="modal.app_name"
+                    render={({ field }) => (
+                      <FormItem className="mb-8">
+                        <FormLabel className="font-bold">App Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            type="text"
+                            placeholder="my-modal-app"
+                            className="w-full"
+                            disabled
+                          />
+                        </FormControl>
+                        <FormDescription>
+                          The name of your Modal App. It is the string inside modal.App() in your
+                          Python file.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                   <ModalFunctions />
                 </div>
               )
