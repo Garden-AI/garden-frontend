3fb0eb21aa8dd0ba3ddd171db077b9e36596a3f6
hholb <hholbrook@uchicago.edu>
2025-03-21 12:51:19 -0400

    show contributors in metadata sidebar

--- garden/src/features/modal/components/ModalFunctionPage.tsx
+++ garden/src/features/modal/components/ModalFunctionPage.tsx
@@ -319,6 +319,16 @@ const ModalFunctionPage = () => {
               ownsThisFunction={ownsThisFunction}
               isArray={true}
             />
+
+            <EditableMetadataField
+              label="Contributors"
+              value={modalFunction.contributors}
+              fieldName="contrubutors"
+              modalFunction={modalFunction}
+              ownsThisFunction={ownsThisFunction}
+              isArray={true}
+            />
+
             <EditableMetadataField
               label="Year"
               value={modalFunction.year}
