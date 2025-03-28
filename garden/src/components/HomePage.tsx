7ad8e595f1a190c704ed1431f3d92720178331d2
hholb <hholbrook@uchicago.edu>
2025-03-28 09:29:10 -0400

    add editablity toggle prop to GardenBox

--- garden/src/components/HomePage.tsx
+++ garden/src/components/HomePage.tsx
@@ -381,7 +381,7 @@ predictions = predict_properties()`}
           <div className="flex justify-center space-x-4 p-4 ">
             {gardens?.map((res: any, index: any) => (
               <div className="h-[300px] w-[300px]" key={index}>
-                <GardenBox garden={res} />
+                <GardenBox garden={res} allowEdits={false} />
               </div>
             ))}
           </div>
