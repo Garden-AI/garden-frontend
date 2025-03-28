7a3a6936cefd02debb43d2581abcf0dde9920cce
hholb <hholbrook@uchicago.edu>
2025-03-28 14:39:50 -0400

    add name param to metadata sidebar

--- garden/src/components/shared/metadata/Metadata.tsx
+++ garden/src/components/shared/metadata/Metadata.tsx
@@ -2,16 +2,17 @@ import { Garden, ModalFunction } from "@/types";
 import { ReactNode } from "react";
 
 interface MetadataProps {
+  name: string | undefined
   entity: Garden | ModalFunction;
   ownsThisEntity: boolean;
   children: ReactNode;
 }
 
-const Metadata = ({ entity, ownsThisEntity, children }: MetadataProps) => {
+const Metadata = ({  name, entity, ownsThisEntity, children }: MetadataProps) => {
   return (
     <div className="lg:w-1/3 bg-gray-50 rounded-lg p-4 border border-gray-200">
       <div className="space-y-2">
-        <h3 className="text-lg font-semibold mb-2">Metadata</h3>
+        <h3 className="text-lg font-semibold mb-2">{name + " " || ""}Metadata</h3>
         {children}
       </div>
     </div>
