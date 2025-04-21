e9b6c5b52423a8f5b9b735f87c497269f4247dfa
hholb <hholbrook@uchicago.edu>
2025-04-21 14:54:03 -0400

    re-hide model deployments tab

--- garden/src/features/users/components/UserProfileTabs.tsx
+++ garden/src/features/users/components/UserProfileTabs.tsx
@@ -32,7 +32,7 @@ const UserProfileTabs = ({ defaultTab = "profile" }: UserProfileTabsProps) => {
         >
           Saved Gardens
         </TabsTrigger>
-        {/* TODO: hidden until feature is ready */}
+        {/* TODO: hidden until feature is ready
         <TabsTrigger
           value="model-deployments"
           className="h-full w-full border-b-4 bg-gray-100 hover:border-green hover:bg-gradient-to-b hover:from-gray-100 hover:from-70% hover:to-green data-[state=active]:border-green data-[state=active]:bg-green data-[state=active]:bg-opacity-30"
@@ -57,7 +57,7 @@ const UserProfileTabs = ({ defaultTab = "profile" }: UserProfileTabsProps) => {
             <SavedGardens />
           </div>
         </TabsContent>
-        {/* TODO: hidden until feature is ready */}
+        {/* TODO: hidden until feature is ready
         <TabsContent value="model-deployments">
           <div className="px-6">
             <ModelDeployments modelDeployments={modelDeployments}></ModelDeployments>
