ced4d7378932683b820b589744141088e1f09a26
hholb <hholbrook@uchicago.edu>
2025-04-23 09:35:09 -0400

    remove comments hiding model deployments tab

--- garden/src/features/users/components/UserProfileTabs.tsx
+++ garden/src/features/users/components/UserProfileTabs.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/shadcn/tabs";
 import UserProfileInfo from "./UserProfileInfo";
 import MyGardens from "./MyGardens";
@@ -32,14 +33,12 @@ const UserProfileTabs = ({ defaultTab = "profile" }: UserProfileTabsProps) => {
         >
           Saved Gardens
         </TabsTrigger>
-        {/* TODO: hidden until feature is ready */}
         <TabsTrigger
           value="model-deployments"
           className="h-full w-full border-b-4 bg-gray-100 hover:border-green hover:bg-gradient-to-b hover:from-gray-100 hover:from-70% hover:to-green data-[state=active]:border-green data-[state=active]:bg-green data-[state=active]:bg-opacity-30"
         >
           Model Deployments
         </TabsTrigger>
-       {/* */}
       </TabsList>
       <div className="min-h-[60vh] flex-grow overflow-auto pt-4 sm:pt-8">
         <TabsContent value="profile">
@@ -57,13 +56,11 @@ const UserProfileTabs = ({ defaultTab = "profile" }: UserProfileTabsProps) => {
             <SavedGardens />
           </div>
         </TabsContent>
-        {/* TODO: hidden until feature is ready */}
         <TabsContent value="model-deployments">
           <div className="px-6">
             <ModelDeployments modelDeployments={modelDeployments}></ModelDeployments>
           </div>
         </TabsContent>
-       {/* */ }
       </div>
     </Tabs>
   );
