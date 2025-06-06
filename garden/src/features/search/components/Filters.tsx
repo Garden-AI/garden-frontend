47b7b93a0f5cb133536bcb9db8c48b46586b87e9
Nathan Houston <lhouston@unca.edu>
2025-06-06 11:28:52 -0400

    Removed old commented logic, Adjusted code formatting

--- garden/src/features/search/components/Filters.tsx
+++ garden/src/features/search/components/Filters.tsx
@@ -9,7 +9,6 @@ import { Checkbox } from "@/components/shadcn/checkbox";
 import { Label } from "@/components/shadcn/label";
 import { Button } from "@/components/shadcn/button";
 import { GardenSearchResult } from "../hooks/useSearchResults";
-
 import { useState } from "react";
 
 const SearchFilters = ({
@@ -37,35 +36,22 @@ const SearchFilters = ({
   if (!facets.length) return null;
 
   return (
-    // <div className="relative h-full">
     <div className="sticky top-16 max-h-[80vh] overflow-y-auto rounded-lg border shadow-md bg-white p-4 space-y-4">
-      {/* <div className="sticky top-12 space-y-4 divide-y-2 rounded-lg border px-4 py-6 shadow-md"> */}
-        {/* <div> */}
-          {/* <h3 className="mb-2 text-lg font-semibold"> */}
           <h3 className="text-lg font-semibold flex items-center mb-2">
-            {/* constructor(parameters) {
-              
-            } */}
-          {/* } */}
-            {/* <Filter className="mr-2 inline-block h-5" /> */}
             <Filter className="mr-2 h-5 w-5" />
             Filters
           </h3>
-
           <Accordion type="multiple" className="px-2" defaultValue={facets.map((f) => f.name)}>
             {facets.map((facet) => {
-
               const buckets = showAllFacets[facet.name]
                 ? facet.values
                 : facet.values.slice(0, 7);
-
               return (
                 <AccordionItem key={facet.name} value={facet.name}>
                   <AccordionTrigger>
                     <Label className="capitalize">{facet.name.split("_").join(" ")}</Label>
                   </AccordionTrigger>
                   <AccordionContent className="pl-2 max-h-48 overflow-y-auto pr-2">
-                    {/* {facet?.values?.slice(0, 7).map((bucket, index) => ( */}
                       {buckets.map((bucket, index) => (
                       <div key={index} className="mb-2 flex items-center gap-2">
                         <Checkbox
@@ -82,7 +68,6 @@ const SearchFilters = ({
                         </Label>
                       </div>
                     ))}
-
                     {facet.values.length > 7 && (
                       <button
                         type="button"
@@ -97,7 +82,6 @@ const SearchFilters = ({
                         {showAllFacets[facet.name] ? "Show Less" : "Show More"}
                       </button>
                     )}
-
                   </AccordionContent>
                 </AccordionItem>
               );
@@ -117,8 +101,6 @@ const SearchFilters = ({
             </Button>
           </div>
         </div>
-      // </div>
-    // </div>
   );
 };
 
