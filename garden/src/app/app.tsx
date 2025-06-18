e24a73a08c3340ec55dfd0e9cdd7f730006fc173
hholb <hholbrook@uchicago.edu>
2025-06-18 12:12:49 -0400

    add react-query-devtools

--- garden/src/app/app.tsx
+++ garden/src/app/app.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { createHashRouter } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Router from "./router";
 
 const App = () => {
   return (
-    <RouterProvider
-      router={createHashRouter([
-        {
-          path: "*",
-          element: <Router />,
-        },
-      ])}
-    />
+    <>
+      <RouterProvider
+        router={createHashRouter([
+          {
+            path: "*",
+            element: <Router />,
+          },
+        ])}
+      />
+      <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+    </>
   );
 };
 
