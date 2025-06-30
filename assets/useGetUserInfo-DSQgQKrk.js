5a675b6a358007aa554f93edd0f7ccc3846899b0
hholb <hholb@users.noreply.github.com>
2025-06-30 16:31:15 +0000

    pull cache population out of 'useQuery{(select: ()=>{}}' callbacks (#393) 1a613151928637009201530d37a86b75acd5657a

--- assets/useGetUserInfo-DSQgQKrk.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./card-DTyRDD9A.js";import{u as s}from"./useQuery-BCRUHGoi.js";import{u as t}from"./index-1aSZ7sxg.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
