3ac4e3ff6f9448955c6f50e8aba0ac867c011f87
Hayden Holbrook <111379706+hholb@users.noreply.github.com>
2025-07-02 11:33:24 -0400

    update hayden's super user id (#396)

--- garden/src/utils/utils.ts
+++ garden/src/utils/utils.ts
@@ -2,5 +2,5 @@ export const SUPER_USERS: string[] = [
     "c8741264-d274-11e5-bee7-f30dff9f1ea8", // Ben
     "6c9e223f-c215-4c26-9abb-262dbce0001c", // Will
     "76024960-c68b-4fec-8cb8-b65b096f18da", // Owen
-    "e9a17e09-657b-4087-a719-241ab72b1d9b", // Hayden
+    "1edf904a-6f76-4a14-8851-d3c55ee0f037", // Hayden
 ];
