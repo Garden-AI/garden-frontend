df309c3d9fdadbbc12ecd0bfc8acf5f1dbd37620
hholb <hholb@users.noreply.github.com>
2025-07-09 18:10:09 +0000

    move all hooks to before early returns in Garden Page (#400) 3b437d9f0b586b2f187a7a816a8e62982d12a3d7

--- assets/useGetModalFunction-Bi3r3FYQ.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./card-a3WHDW-r.js";import{u as a}from"./useQuery-CQ2ZQGLY.js";const u=n=>a({queryKey:["modalFunctions",Number(n)],queryFn:async()=>{const o=(await t.get(`/modal-functions/${n}`)).data,e=await t.get(`/modal-apps/${o.modal_app_id}`);return{...o,owner_identity_id:e.data.owner_identity_id}}});export{u};
