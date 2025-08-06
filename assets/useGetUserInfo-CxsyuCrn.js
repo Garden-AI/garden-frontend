c9130935617afb213fcbb559a4c3ff8c01d18ab2
WillEngler <WillEngler@users.noreply.github.com>
2025-08-06 22:49:01 +0000

    Rough draft of MLIP Page (#411) 9d27c841e8c16257ddca6d794b0c88361fc3ea69

--- assets/useGetUserInfo-CxsyuCrn.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./card-DyeZZLCo.js";import{u as s}from"./useQuery-Yhqnxfja.js";import{b as t}from"./index-CEuAe3ga.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
