dcb47b5846319aec825bd982773b8b43a2967120
WillEngler <WillEngler@users.noreply.github.com>
2025-08-08 15:00:47 +0000

    More MLIP copy and layout changes (#414) 0b85060c289ebf1aea8ca7d6dafa78740567f79e

--- assets/database-CQQvatpy.js
+++
@@ -1,6 +0,0 @@
-import{h as e}from"./index-9U6Fos3H.js";/**
- * @license lucide-react v0.394.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const s=e("Database",[["ellipse",{cx:"12",cy:"5",rx:"9",ry:"3",key:"msslwz"}],["path",{d:"M3 5V19A9 3 0 0 0 21 19V5",key:"1wlel7"}],["path",{d:"M3 12A9 3 0 0 0 21 12",key:"mv7ke4"}]]);export{s as D};
