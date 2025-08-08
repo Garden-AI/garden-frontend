a3382fd9f316338d1fac97806c6110808931f29c
WillEngler <WillEngler@users.noreply.github.com>
2025-08-08 19:38:00 +0000

    pre-demo tweaks (#416) cf7833dd526615052815c7b3cea7092d8ca37f86

--- assets/useGetUserInfo-BA2F0C9-.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./axios-m_-IxfSB.js";import{u as s}from"./useQuery-7MdSSCCp.js";import{b as t}from"./index-2NiM7tRl.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
