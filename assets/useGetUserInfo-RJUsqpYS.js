afd4362ab01089bb6e5d6c3a65ba5a4c0483b6ae
WillEngler <WillEngler@users.noreply.github.com>
2025-08-13 14:59:03 +0000

    update with actual 7net time (#423) 830cd83c5864389369c4d4296bf76293436f7218

--- assets/useGetUserInfo-RJUsqpYS.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./card-Bnvjvp7_.js";import{u as s}from"./useQuery-CFTb2-TI.js";import{b as t}from"./index-Bd0qmTq9.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
