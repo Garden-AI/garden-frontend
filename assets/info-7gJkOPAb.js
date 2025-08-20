64618bab1a9befc8e87491a71fb1b52d7b706aa6
hholb <hholb@users.noreply.github.com>
2025-08-20 19:26:24 +0000

    Bugfixes and accessibility sweep  for MLIP Landing Page (#437) 0ab5ee408a2a1806637084d9005c97507c5d9ef3

---
+++ assets/info-7gJkOPAb.js
@@ -0,0 +1,6 @@
+import{h as e}from"./index-D0vN-FyA.js";/**
+ * @license lucide-react v0.394.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=e("Info",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M12 16v-4",key:"1dtifu"}],["path",{d:"M12 8h.01",key:"e9boi3"}]]);export{o as I};
---
+++ assets/trash-BzD4M86W.js
@@ -0,0 +1,11 @@
+import{h as e}from"./index-D0vN-FyA.js";/**
+ * @license lucide-react v0.394.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("CircleCheckBig",[["path",{d:"M22 11.08V12a10 10 0 1 1-5.93-9.14",key:"g774vq"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]]);/**
+ * @license lucide-react v0.394.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=e("Trash",[["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6",key:"4alrt4"}],["path",{d:"M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2",key:"v07s0e"}]]);export{c as C,h as T};
