c4cb4f80d42b0fc4c14273b25aa9a1107c52a66d
hholb <hholbrook@uchicago.edu>
2025-10-07 11:25:51 -0600

    remove used file

--- garden/src/features/hpc-admin/components/index.ts
+++ garden/src/features/hpc-admin/components/index.ts
@@ -2,4 +2,3 @@ export { default as HpcAdminDashboard } from "./HpcAdminDashboard";
 export { CreateHpcEndpointForm } from "./CreateHpcEndpointForm";
 export { CreateHpcDeploymentForm } from "./CreateHpcDeploymentForm";
 export { CreateHpcFunctionForm } from "./CreateHpcFunctionForm";
-export { AssociateHpcFunctionForm } from "./AssociateHpcFunctionForm";
