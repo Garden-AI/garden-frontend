11df571230a9f36110874060db420ab5b4d9ac94
hholb <hholb@users.noreply.github.com>
2025-10-10 19:17:11 +0000

    Merge pull request #449 from Garden-AI/hholb/use-cases
    Create use cases section on home page, add navbar link 8a668d7c071001197bf99c78f132b176bac23d1c

--- assets/search-DLjOsiwJ.js
+++
@@ -1,6 +0,0 @@
-import{h as e}from"./index-BjPYaz21.js";/**
- * @license lucide-react v0.394.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const r=e("Search",[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]]);export{r as S};
